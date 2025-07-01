db74e5de68962053739a1e770bedc313f79d2da6
Sumit00s <sonawanesumit245@gmail.com>
2025-07-01 12:25:42 +0530

    Neon Setup Successfuly

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+async function initDB() {
+    try{
+        await sql `CREATE TABLE IF NOT EXISTS transactions(
+         id SERIAL PRIMARY KEY,
+         user_id VARCHAR(255) NOT NULL,
+         title VARCHAR(255) NOT NULL,
+         amount DECIMAL(10,2) NOT NULL,
+         category VARCHAR(255) NOT NULL,
+         created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+
+        console.log("Databse Initialed successfully");
+    }catch(error){
+        console.log("Error while creating DB",error);
+        process.exit(1)
+    }
+}
+
+app.get("/",(req,res)=>{
+    res.send("Its working");
+})
+
+initDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is Running on PORT: ${PORT}`)
+    });
+});
